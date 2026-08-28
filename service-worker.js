b527a8f300293a9f540c68540a175e19b5335e96
avsecSRG <srg.os@injourneyairports.id>
2026-08-28 18:03:19 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'elogbook-airport-security-v1';
+const CACHE_NAME = 'elogbook-airport-security-v3';
 
 const APP_SHELL = [
   './',
@@ -6,130 +6,72 @@ const APP_SHELL = [
   './manifest.json',
 
   './assets/app-icon.png',
+
   './assets/home-hp.png',
   './assets/home-tablet.png',
-  './assets/home-monitor.png'
+  './assets/home-monitor.png',
+
+  './assets/daily-hp.png',
+  './assets/daily-tablet.png',
+  './assets/daily-monitor.png',
+
+  './assets/icon-192.png',
+  './assets/icon-512.png'
 ];
 
-
 self.addEventListener('install', event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => {
-
-        return cache.addAll(APP_SHELL);
-
-      })
-
-      .then(() => {
-
-        return self.skipWaiting();
-
-      })
-
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
   );
-
 });
-
 
 self.addEventListener('activate', event => {
-
   event.waitUntil(
-
-    caches.keys()
-      .then(cacheNames => {
-
-        return Promise.all(
-
-          cacheNames
-            .filter(name => name !== CACHE_NAME)
-
-            .map(name => {
-
-              return caches.delete(name);
-
-            })
-
-        );
-
-      })
-
-      .then(() => {
-
-        return self.clients.claim();
-
-      })
-
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
-
 });
 
-
 self.addEventListener('fetch', event => {
-
   if (event.request.method !== 'GET') {
     return;
   }
 
-
   event.respondWith(
-
     caches.match(event.request)
-
       .then(cachedResponse => {
-
         if (cachedResponse) {
           return cachedResponse;
         }
 
-
         return fetch(event.request)
-
           .then(networkResponse => {
 
             if (
-              !networkResponse ||
-              networkResponse.status !== 200 ||
-              networkResponse.type === 'opaque'
+              networkResponse &&
+              networkResponse.status === 200 &&
+              networkResponse.type === 'basic'
             ) {
+              const responseClone = networkResponse.clone();
 
-              return networkResponse;
-
+              caches.open(CACHE_NAME)
+                .then(cache => {
+                  cache.put(event.request, responseClone);
+                });
             }
 
-
-            const responseClone =
-              networkResponse.clone();
-
-
-            caches.open(CACHE_NAME)
-
-              .then(cache => {
-
-                cache.put(
-                  event.request,
-                  responseClone
-                );
-
-              });
-
-
             return networkResponse;
-
           })
-
           .catch(() => {
-
-            return caches.match(
-              './index.html'
-            );
-
+            return caches.match('./index.html');
           });
-
       })
-
   );
-
 });
